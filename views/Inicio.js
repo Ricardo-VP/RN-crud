d29e641eb15cee897e501994da1ed0db7f4bc4f9
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-05 00:26:31 -0500

    Obteniendo los clientes de la API

--- views/Inicio.js
+++ views/Inicio.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native';
+import axios from 'axios';
 
 const Inicio = () => {
+
+    // State de la app
+    const [clientes, setClientes] = useState([]);
+
+    useEffect(() => {
+        const obtenerClientesdeApi = async () => {
+            try {
+                // SOLO android
+                const resultado = await axios.get('http://10.0.2.2:3000/clientes');
+                setClientes(resultado.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        obtenerClientesdeApi(); 
+    }, []);
+
     return ( 
         <Text>Inicio</Text>
     );
