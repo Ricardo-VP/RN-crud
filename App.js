01354522158952778587bf94b123a114aca69c8b
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-04 20:44:44 -0500

    Creando la navegacion y componente Inicio

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
@@ -9,11 +10,28 @@ import {
   View,
 } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Inicio from './views/Inicio';
+
+const Stack = createStackNavigator();
+
 const App = () => {
  
   return (
     <>
+      <NavigationContainer>
+        <Stack.Navigator 
+          initialRouteName="Inicio"
+        >
+          <Stack.Screen 
+            name="Inicio"
+            component={Inicio}
+          />
 
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
