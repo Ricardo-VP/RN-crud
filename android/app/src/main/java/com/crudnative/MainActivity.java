1c8ea4973a55356d970e71788f977627e097095d
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-04 20:39:08 -0500

    Instalando react navigation stack

--- android/app/src/main/java/com/crudnative/MainActivity.java
+++ android/app/src/main/java/com/crudnative/MainActivity.java
@@ -1,6 +1,7 @@
 package com.crudnative;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,9 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "crudnative";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
